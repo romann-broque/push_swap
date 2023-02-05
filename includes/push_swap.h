ee2cba3c5b62699fc76f16b15cda3d0d1a808226
romann-broque <kinoide56@gmail.com>
2023-02-05 16:26:32 +0100

    fact in progress

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: rbroque <rbroque@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/29 00:29:18 by rbroque           #+#    #+#             */
-/*   Updated: 2023/02/03 16:55:25 by rbroque          ###   ########.fr       */
+/*   Updated: 2023/02/03 18:11:00 by rbroque          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -127,6 +127,7 @@ t_list	*gen_list(t_stack *numbers, const size_t count);
 
 // list_utils.c
 
+void	ft_lstinsert(t_list **head, void *content);
 void	ft_lstremove(t_list **head, void (*free_fct)(void *));
 void	ft_contadd_back(t_list **lst, void *content);
 void	ft_lstprint(const t_list *root);
--- srcs/dualstack/fact_instructions.c
+++ srcs/dualstack/fact_instructions.c
@@ -6,13 +6,23 @@
 /*   By: rbroque <rbroque@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 01:37:25 by rbroque           #+#    #+#             */
-/*   Updated: 2023/02/03 17:25:46 by rbroque          ###   ########.fr       */
+/*   Updated: 2023/02/05 16:26:12 by rbroque          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static void	remove_useless_push(t_list *parent)
+void	pre_remove_useless_push(t_list **parent)
+{
+	if (ft_lstsize(*parent) >= 2
+		&& (ft_strcmp((*parent)->content, "pb") == 0 && ft_strcmp((*parent)->next->content, "pa") == 0))
+	{
+		ft_lstremove(parent, NULL);
+		ft_lstremove(parent, NULL);
+	}
+}
+
+void	remove_useless_push(t_list *parent)
 {
 	t_list	*tmp;
 
@@ -34,33 +44,9 @@ static void	remove_useless_push(t_list *parent)
 	}
 }
 
-void	remove_useless_pushswap(t_list *parent)
-{
-	t_list	*tmp;
+////////////
 
-	while (ft_lstsize(parent) > 3)
-	{
-		if (ft_strcmp(parent->next->content, "pb") == 0
-			&& ft_strcmp(parent->next->next->content, "pb") == 0
-			&& ft_strcmp(parent->next->next->next->content, "sb") == 0)
-		{
-			ft_lstremove(&parent, NULL);
-			ft_lstremove(&(parent->next), NULL);
-			ft_lstremove(&(parent->next->next), NULL);
-			//ft_lstinsert(parent, "sa");
-			break ;
-		}
-		else
-		{
-			tmp = parent->next->next;
-			remove_useless_push(parent->next);
-			if (tmp == parent->next->next)
-				break ;
-		}
-	}
-}
-
-void	fact_instructions(t_list **parent)
+void	pre_remove_useless_pushswap(t_list **parent)
 {
 	if (ft_lstsize(*parent) >= 3
 		&& ft_strcmp((*parent)->content, "pb") == 0
@@ -69,16 +55,110 @@ void	fact_instructions(t_list **parent)
 	{
 		ft_lstremove(parent, NULL);
 		ft_lstremove(parent, NULL);
-		ft_lstremove(parent, NULL);
-		//insert("sb")
+		(*parent)->content = "sa";
 	}
-	if (ft_lstsize(*parent) >= 2
-		&& (ft_strcmp((*parent)->content, "pb") == 0 && ft_strcmp((*parent)->next->content, "pa") == 0))
+}
+
+void	remove_useless_pushswap(t_list **parent)
+{
+	t_list	*tmp;
+
+	while (ft_lstsize(*parent) > 3)
 	{
-		ft_lstremove(parent, NULL);
-		ft_lstremove(parent, NULL);
+		if (ft_strcmp((*parent)->next->content, "pb") == 0
+			&& ft_strcmp((*parent)->next->next->content, "pb") == 0
+			&& ft_strcmp((*parent)->next->next->next->content, "sb") == 0)
+		{
+			ft_lstremove(parent, NULL);
+			ft_lstremove(&((*parent)->next), NULL);
+			(*parent)->next->content = "sa";
+			break ;
+		}
+		else
+		{
+			tmp = (*parent)->next->next;
+			remove_useless_pushswap(&((*parent)->next));
+			if (tmp == (*parent)->next->next)
+				break ;
+		}
 	}
+}
+
+//////////////////
+
+static t_list	*ft_lstget(t_list *head, const size_t index)
+{
+	size_t	i;
+
+	i = 0;
+	while (head != NULL && i < index)
+	{
+		head = head->next;
+		++i;
+	}
+	return (head);
+}
+
+static size_t	get_index_from_str(t_list *parent, const char *str)
+{
+	size_t	index;
+
+	index = 0;
+	while (parent != NULL && ft_strcmp(parent->content, str) != 0)
+	{
+		++index;
+		parent = parent->next;
+	}
+	return (index);
+}
+
+static size_t	count_list_occurence(t_list *parent, const char *str)
+{
+	size_t	count;
+
+	count = 0;
+	while (parent != NULL)
+	{
+		if (ft_strcmp(parent->content, str) == 0)
+			++count;
+		parent = parent->next;
+	}
+	return (count);
+}
+
+static void	remove_last_pa(t_list **parent)
+{
+	if (*parent != NULL)
+	{
+		ft_lstclear(&((*parent)->next), NULL);
+		(*parent)->next = NULL;
+	}
+}
+
+void	remove_useless_pushback(t_list **parent)
+{
+	const size_t	count_pb = count_list_occurence(*parent, "pb");
+	const size_t	index_pa = get_index_from_str(*parent, "pa");
+	t_list			*begin_pa;
+
+	if (index_pa + count_pb == 0)
+		ft_lstclear(parent, NULL);
+	else
+	{
+		begin_pa = ft_lstget(*parent, index_pa + count_pb - 1);
+		remove_last_pa(&begin_pa);
+	}
+}
+
+///////////////////
+
+void	fact_instructions(t_list **parent)
+{
+	print_instructions(*parent);
+	printf("<------->\n");
+	pre_remove_useless_pushswap(parent);
+	remove_useless_pushswap(parent);
+	pre_remove_useless_push(parent);
 	remove_useless_push(*parent);
-	remove_useless_pushswap(*parent);
-	//remove useless push_back
+	remove_useless_pushback(parent);
 }
